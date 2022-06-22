b4d3d2f26bec8a04dd5ea36a020373ee7bc45fb2
matheus <matheusaveroro@gmail.com>
2022-06-21 21:45:30 -0300

    sintaxe
    sintaxe básica do PHP

---
+++ base_sintaxe.php
@@ -0,0 +1,33 @@
+<?php
+
+echo 'fafafafafa';
+
+$variavelqualquer = 10;
+
+define('variavel_qualquer', 10);
+
+//      conetario de uma linha
+#       comentário de uma linha
+
+/*      cometario
+        de varias
+        linhas
+*/
+
+/*    OPERADORES ARITIMÉTICOS
+           + - * / %
+
+
+      OPERADORES RELACIONAIS
+           > < == != 
+           
+           
+      OPERADORES LÓGICOS
+           AND  OR  NOT
+
+*/ 
+
+//TERMINAL
+$variavel = readline();
+
+?>
